b80db70ec4fbe454aba46b54da36c2cb9e99fee3
mvrissaleong <mvrissaleongis@gmail.com>
2025-05-23 00:16:43 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import LandingPage from './components/LandingPage'
+import Affirmations from './components/Affirmations'
+import Music from './components/Music'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="container">
+      <LandingPage />
+      <Affirmations />
+      <Music />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
