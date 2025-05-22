21abc939b9bc42041cf404495072827b601c2d6e
mvrissaleong <mvrissaleongis@gmail.com>
2025-05-23 00:17:50 +0100

    Create LandingPage.jsx

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+  return (
+    <div>
+      <img src="/piplup.gif" alt="Piplup" className="cute-img" />
+      <h1>Hi 宝！</h1>
+      <p>This is your little happy space — wherever you are.</p>
+    </div>
+  )
+}
