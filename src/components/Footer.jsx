9f2f40f98e35eb3cfb25aea82b76bee301a8258e
mvrissaleong <mvrissaleongis@gmail.com>
2025-05-23 00:21:29 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ marginTop: "3rem", fontSize: "0.8rem", color: "#999" }}>
+      Made with lotsa love by ur favourite girl <3
+    </footer>
+  )
+}
